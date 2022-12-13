d0c9452fa497b63b8b71c3f24aae2244dff987e5
Mateus <86882032+Leykon7@users.noreply.github.com>
2022-12-13 15:50:28 -0400

    segundo commit

--- intereface kivy banco de dados/main.py
+++ intereface kivy banco de dados/main.py
@@ -1,4 +1,3 @@
-import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
@@ -8,7 +7,18 @@ class MyWidget(BoxLayout):
         """
         Método simples para incremento do valor mostrado no label
         """
-        self.ids.lb.text = str(int(self.ids.lb.text) + 1) 
+        self.ids.lb.text = str(int(self.ids.lb.text) + 1)
+
+    def pesquisar(self):
+        inicial = str(self.ids.datai.text) + ' ' + str(self.ids.horai.text)
+        final = str(self.ids.dataf.text) + ' ' + str(self.ids.horaf.text)
+        #Teste do textinput 
+        print(f'Entrada inicial: {inicial}\n')
+        print(f'Entrada final: {final}\n')
+
+        #Adicionar lógica do Banco de dados
+    
+
 
 class InterfaceApp(App):
     def build(self):
